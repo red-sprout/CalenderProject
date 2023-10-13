ecbe12941e8979f820b98a0407faa0c4c069bbe9
red-sprout <jjw05015@gmail.com>
2023-10-13 17:52:32 +0900

    add img tag

--- js/background.js
+++ js/background.js
@@ -1,10 +1,7 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const backGroundImages = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+const chosenImage =
+  backGroundImages[Math.floor(Math.random() * backGroundImages.length)];
+const imgTag = document.createElement("img");
+imgTag.src = `img/${chosenImage}`;
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-console.log(bgImage);
-
-document.body.appendChild(bgImage);
+document.body.appendChild(imgTag);
