7139094d51d32b7211fa605c4743c80b47e88d82
red-sprout <jjw05015@gmail.com>
2023-10-15 01:12:54 +0900

    add AM & PM

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,32 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("#clock span");
+const Am = document.querySelector(".ampm #am");
+const Pm = document.querySelector(".ampm #pm");
 
-function getClock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0");
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+function getClock() {
+  const date = new Date();
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  if (hours > 12) {
+    hours -= 12;
+    changeAmPm(0.3, 1);
+  } else if (hours == 12) {
+    changeAmPm(0.3, 1);
+  } else if (hours == 0) {
+    hours = 12;
+    changeAmPm(1, 0.3);
+  } else {
+    changeAmPm(1, 0.3);
+  }
+
+  hours = String(hours).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function changeAmPm(amOpacity, pmOpacity) {
+  Am.style.opacity = amOpacity;
+  Pm.style.opacity = pmOpacity;
 }
 
 getClock();
