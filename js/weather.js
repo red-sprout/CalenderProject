2581decefea29db1b88558d21cb12f70ed4e9f5b
red-sprout <jjw05015@gmail.com>
2023-10-15 01:14:56 +0900

    add icons

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,24 @@
+const weather = document.querySelector("#weather");
+const weatherImg = document.createElement("img");
+const tempInfo = document.createElement("span");
+weather.appendChild(weatherImg);
+weather.appendChild(tempInfo);
+
 const API_KEY = "2e63553c8e5ac238929fc02d02a45d2c";
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json().then(data => {
-        const weather = document.querySelector("#weather span:nth-child(1)");
-        const city = document.querySelector("#weather span:nth-child(2)");
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        city.innerText = data.name;
-    }));
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      weatherImg.src = `img/weather/${data.weather[0].main}.png`;
+      tempInfo.innerText = `\n${data.main.temp}°C`;
+    })
+  );
 }
 
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
+function onGeoError() {
+  tempInfo.innerText = "No information";
 }
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
