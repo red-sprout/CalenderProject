ccd81dc8cd37385e8ab38a8325ac16de326ff8aa
red-sprout <jjw05015@gmail.com>
2023-10-15 16:01:16 +0900

    D-day, 끄기 기능 추가

--- js/dday.js
+++ js/dday.js
@@ -3,10 +3,38 @@ const ddayForm = ddayClass.querySelector("#dday-form");
 const ddayInput = ddayForm.querySelector("input");
 const ddayList = ddayClass.querySelector("#dday-list");
 const xdday = ddayClass.querySelector("#dday-close");
+const openDday = document.querySelector("#open-dday");
 
 const DDAYS_KEY = "ddays";
 
 let ddays = [];
+
+const one_day = 1000 * 60 * 60 * 24;
+let ddayStr = "";
+
+function getClock(newdday) {
+  const date = new Date();
+  const year = newdday[0] + newdday[1] + newdday[2] + newdday[3];
+  const month = newdday[5] + newdday[6];
+  const day = newdday[8] + newdday[9];
+  console.log(year + month + day);
+  const targetDday = new Date(
+    parseInt(year, 10),
+    parseInt(month, 10) - 1,
+    parseInt(day)
+  );
+
+  const days = Math.ceil((targetDday.getTime() - date.getTime()) / one_day);
+  if (days == 0) {
+    ddayStr = "D-day";
+  } else if (days < 0) {
+    ddayStr = `D+${Math.abs(days)}`;
+  } else if (days > 0) {
+    ddayStr = `D-${days}`;
+  } else {
+    ddayStr = "";
+  }
+}
 
 function saveddays() {
   localStorage.setItem(DDAYS_KEY, JSON.stringify(ddays));
@@ -28,7 +56,11 @@ function paintdday(newdday) {
   const li = document.createElement("li");
   li.id = newdday.id;
   const span = document.createElement("span");
-  span.innerText = newdday.text;
+  const ddayTarget = newdday.text;
+  getClock(ddayTarget);
+  setInterval(getClock(ddayTarget), 1000);
+  span.innerText = `(${ddayStr}) ${newdday.text}`;
+  ddayStr = "";
   const button = document.createElement("img");
   button.src = "img/icon/trash.png";
   button.addEventListener("click", deletedday);
@@ -51,8 +83,14 @@ function handleddaySubmit(event) {
   saveddays();
 }
 
+function handleOpenDday(event) {
+  event.preventDefault();
+  ddayClass.classList.toggle(HIDDEN_CLASSNAME);
+}
+
 ddayForm.addEventListener("submit", handleddaySubmit);
 xdday.addEventListener("click", closedday);
+openDday.addEventListener("click", handleOpenDday);
 
 const savedddays = localStorage.getItem(DDAYS_KEY);
 if (savedddays !== null) {
